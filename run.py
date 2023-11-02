1f7b0812bcf2ece98ef196e3487fa13dcb545c8b
Ivan <petrovic.valpovo@gmail.com>
2023-11-02 19:50:53 +0000

    Add welcome message with select screen and name input

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to Who Wants To Be A Millionaire")
+select_screen = input("Select:\n(Play|Rules)\n")
+if select_screen == "Play":
+    player_name = input("Enter Your Name: ")
+    print("Here..")
+    print("We..")
+    print("Go!")
+elif select_screen == "Rules":
+    print("Rules for 'Who Wants to Be a Millionaire?' Game:")
+    print("Objective: The objective of the game is to answer a series of multiple-choice questions correctly to accumulate as much money as possible.\n")
