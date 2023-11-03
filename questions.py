89e3bd640aa7f1f93191f766f14d4ed96fe7c1fc
Ivan <petrovic.valpovo@gmail.com>
2023-11-03 13:56:13 +0000

    Add questions,play_round function which provides basic game logic and main function

---
+++ questions.py
@@ -0,0 +1,52 @@
+easy_questions = [
+    {
+        'question': 'What is the capital of France?',
+        'choices': ['A) Madrid', 'B) Berlin', 'C) Paris', 'D) Rome'],
+        'answer': 'C',
+    },
+    {
+        'question': 'How many continents are there on Earth?',
+        'choices': ['A) 5', 'B) 6', 'C) 7', 'D) 8'],
+        'answer': 'C',
+    },
+    {
+        'question': 'Which gas do plants absorb from the atmosphere?',
+        'choices': ['A) Oxygen', 'B) Carbon Dioxide', 'C) Hydrogen', 'D) Nitrogen'],
+        'answer': 'B',
+    },
+    {
+        'question': 'What is the primary color that results from mixing red and blue?',
+        'choices': ['A) Red', 'B) Blue', 'C) Green', 'D) Yellow'],
+        'answer': 'C',
+    },
+    {
+        'question': 'Which planet is known as the "Red Planet"?',
+        'choices': ['A) Earth', 'B) Mars', 'C) Venus', 'D) Jupiter'],
+        'answer': 'B',
+    },
+    {
+        'question': 'What is the largest mammal in the world?',
+        'choices': ['A) Elephant', 'B) Blue Whale', 'C) Giraffe', 'D) Lion'],
+        'answer': 'B',
+    },
+    {
+        'question': 'Which bird is known for its colorful plumage and can mimic human speech?',
+        'choices': ['A) Penguin', 'B) Parrot', 'C) Crow', 'D) Hawk'],
+        'answer': 'B',
+    },
+    {
+        'question': 'What is the chemical symbol for water?',
+        'choices': ['A) H2O', 'B) CO2', 'C) O2', 'D) NaCl'],
+        'answer': 'A',
+    },
+    {
+        'question': 'Which country is famous for its pyramids and the Nile River?',
+        'choices': ['A) Italy', 'B) China', 'C) Egypt', 'D) Brazil'],
+        'answer': 'C',
+    },
+    {
+        'question': 'What is the largest land mammal in the world?',
+        'choices': ['A) Rhino', 'B) Elephant', 'C) Lion', 'D) Tiger'],
+        'answer': 'B',
+    },
+]
--- run.py
+++ run.py
@@ -1,10 +1,46 @@
-print("Welcome to Who Wants To Be A Millionaire")
-select_screen = input("Select:\n(Play|Rules)\n")
-if select_screen == "Play":
-    player_name = input("Enter Your Name: ")
-    print("Here..")
-    print("We..")
-    print("Go!")
-elif select_screen == "Rules":
-    print("Rules for 'Who Wants to Be a Millionaire?' Game:")
-    print("Objective: The objective of the game is to answer a series of multiple-choice questions correctly to accumulate as much money as possible.\n")
+import random
+from questions import easy_questions
+random.shuffle(easy_questions)
+
+def main():
+    """
+    Main function with welcome screen and selection
+    Starts the game after user inputs 'Play'
+    """
+    print("Welcome to Who Wants To Be A Millionaire")
+    select_screen = input("Select:\n(Play|Rules)\n")
+    if select_screen == "Play":
+        player_name = input("Enter Your Name: ")
+        print("Here..")
+        print("We..")
+        print("Go!")
+        play_round()  # Game starts 
+    elif select_screen == "Rules":
+        print("Rules for 'Who Wants to Be a Millionaire?' Game:")
+        print("Objective: The objective of the game is to answer a series of multiple-choice questions correctly to accumulate as much money as possible.\n")
+
+def play_round():
+    """
+    Game logic for when the game starts
+    Displays question with the set of choices
+    On wrong response, player is provided with the correct answer
+    """
+    for question_data in easy_questions:  # Easy questions from questions.py stored into variables
+        easy_question = question_data['question']
+        easy_choices = question_data['choices']
+        easy_answer = question_data['answer']
+    
+        print(easy_question) 
+        for choice in easy_choices: 
+            print(choice)
+    
+        player_answer = input("Choose an answer (A, B, C or D): ")
+
+        if player_answer == easy_answer:  # Statement to check if user answer matches
+            print("Correct")
+        else:
+            print(f"Wrong answer\nCorrect answer was: {easy_answer}")
+        break
+
+main()
+print("Thank you for playing, better luck next time!")
