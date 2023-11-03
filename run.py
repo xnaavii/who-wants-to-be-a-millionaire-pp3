9be822c7eb46680179f904e3f10ed554c2d02a30
Ivan <petrovic.valpovo@gmail.com>
2023-11-03 17:03:09 +0000

    Add new lines between print statements

--- run.py
+++ run.py
@@ -9,15 +9,16 @@ def main():
     Main function with welcome screen and selection
     Starts the game after user inputs 'Play'
     """
-    print("Welcome to Who Wants To Be A Millionaire")
+    print("\nWelcome to Who Wants To Be A Millionaire")
     time.sleep(0.5)
-    select_screen = input("Select:\n(Play|Rules)\n")
+    print("\nSelect")
+    select_screen = input("\n[Play]|[Rules]: ")
     time.sleep(0.5)
     if select_screen == "Play":
-        player_name = input("Enter Your Name: ")
-        print("Here..")
+        player_name = input("\nEnter Your Name: ")
+        print("\nHere..\n")
         time.sleep(0.5)
-        print("We...")
+        print("We...\n")
         time.sleep(0.5)
         print("Go!\n")
         play_round()  # Game starts 
@@ -31,7 +32,6 @@ def play_round():
     Displays question with the set of choices
     On wrong response, player is provided with the correct answer
     """
-    
     score = 0
     for question_data in easy_questions:  # Easy questions from questions.py stored into variables
         easy_question = question_data['question']
@@ -42,25 +42,27 @@ def play_round():
         time.sleep(1)
         for choice in easy_choices: 
             print(f"{choice}\n")
-            time.sleep(1.2)
+            time.sleep(1)
         
         player_answer = get_players_answer()
         
         if player_answer == easy_answer:  # Statement to check if user answer matches
+            print("...")
+            time.sleep(1)
+            print("....")
+            time.sleep(1)
+            score += 100
+            time.sleep(0.5)
+            print(f"\nCorrect! Your score is {score}$\n")
+            continue
+        else:
             print("...\n")
             time.sleep(1)
             print("....\n")
-            time.sleep(2)
-            print("Correct\n")
-            score += 100
-            time.sleep(0.5)
-            continue 
-        else:
-            print(f"Wrong answer\nCorrect answer was: {easy_answer}")
-            
+            time.sleep(1)
+            print(f"\nWrong answer\nCorrect answer was: {easy_answer}\n")
         break
-
     print("Thank you for playing, better luck next time!\n")
-    print(f"Your score was: {score}$")
+    print(f"Your score was: {score}$\n")
 
 main()
