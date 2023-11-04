e966881135b4a17239c6153a21b2d98e0970b8ef
Ivan <petrovic.valpovo@gmail.com>
2023-11-04 13:19:15 +0100

    Add feature to show player name on the right answer

--- run.py
+++ run.py
@@ -14,7 +14,6 @@ def main():
     select_screen = input("\n[Play]|[Rules]: ")
     time.sleep(0.5)
     if select_screen == "Play":
-        player_name = input("\nEnter Your Name: ")
         print("\nHere..\n")
         time.sleep(0.5)
         print("We...\n")
@@ -32,13 +31,15 @@ def play_round():
     On wrong response, player is provided with the correct answer
     """
     random.shuffle(easy_questions)
+    player_name = input("Enter Your Name: ")
     score = 0
     for question_data in easy_questions:  # Easy questions from questions.py stored into variables
         easy_question = question_data['question']
         easy_choices = question_data['choices']
         easy_answer = question_data['answer']
-    
-        print(f"{easy_question}\n") 
+
+        time.sleep(1)
+        print(f"\n{easy_question}\n") 
         time.sleep(1)
         for choice in easy_choices: 
             print(f"{choice}\n")
@@ -47,13 +48,14 @@ def play_round():
         player_answer = get_players_answer()
         
         if player_answer == easy_answer:  # Statement to check if user answer matches
-            print("...")
+            print("...\n")
             time.sleep(1)
-            print("....")
+            print("....\n")
             time.sleep(1)
             score += 100
             time.sleep(0.5)
-            print(f"\nCorrect! Your score is {score}$\n")
+            print(f"\nCorrect answer, {player_name}! Your score is {score}$\n") # Show player name and score during gameplay
+            time.sleep(0.5)
             continue
         else:
             print("...\n")
