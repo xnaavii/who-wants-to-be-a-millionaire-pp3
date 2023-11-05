5272e579ec79921cafe8069c9abffdf2a4641dd9
Ivan <petrovic.valpovo@gmail.com>
2023-11-05 21:24:24 +0100

    Add statement to check if all answers are correct

--- game/main.py
+++ game/main.py
@@ -9,14 +9,14 @@ def main():
     Starts the game after user inputs 'Play'
     """
     print("\nWelcome to Who Wants To Be A Millionaire")
-    time.sleep(0.5)
+    time.sleep(0.3)
     select_screen = play_rules()
-    time.sleep(0.5)
+    time.sleep(0.3)
     if select_screen == "Play":
         print("\nHere..\n")
-        time.sleep(0.5)
+        time.sleep(0.3)
         print("We...\n")
-        time.sleep(0.5)
+        time.sleep(0.3)
         print("Go!\n")
         game_setup() # Game starts 
     elif select_screen == "Rules":
@@ -37,7 +37,8 @@ def play_round(player_name, difficulty="Easy"):
     On wrong response, player is provided with the correct answer
     """
     score = 0
-    questions = 0
+    questions = []
+    correct_answers = 0
     # Questions are extracted based on game difficulty
     if difficulty == "Easy":
         questions = easy_questions
@@ -47,45 +48,52 @@ def play_round(player_name, difficulty="Easy"):
         questions = hard_questions
     
     random.shuffle(questions)
+
     for question_data in questions: # Questions from questions.py stored into variables
         question = question_data['question']
         choices = question_data['choices']
         answer = question_data['answer']
 
-        time.sleep(0.8)
+        time.sleep(0.5)
         print(f"\n{question}\n") 
-        time.sleep(0.8)
+        time.sleep(0.5)
         for choice in choices: 
             print(f"{choice}\n")
-            time.sleep(0.8)
+            time.sleep(0.5)
         
         player_answer = get_players_answer()
         
         if player_answer == answer:  # Statement to check if user answer matches
             print("...\n")
-            time.sleep(0.5)
+            time.sleep(0.3)
             print("....\n")
-            time.sleep(0.5)
+            time.sleep(0.3)
             score += 100
-            time.sleep(0.5)
+            correct_answers += 1
+            time.sleep(0.3)
             print(f"Correct answer, {player_name}! Your score is {score}$\n") # Show player name and score during gameplay
             time.sleep(0.5)
             continue
-        else:
+        elif player_answer != answer:
             print("...\n")
-            time.sleep(0.5)
+            time.sleep(0.3)
             print("....\n")
-            time.sleep(0.5)
-            print(f"\nWrong answer\n\nCorrect answer was: {answer}\n")
+            time.sleep(0.3)
+            print(f"\nWrong answer\n\nC33333mfsdklmckladmkdlamlkamlaoirrect answer was: {answer}\n")
         break
-    print("Thank you for playing, better luck next time!\n")
+    
+    if correct_answers == len(questions): # Checks if all questions are answered
+        print("You're a millionaire!\n")
+
+    print("Thank you for playing!\n")
     print(f"Your score was: {score}$\n")
+    
     again = play_again(player_name) # Validate user input on play again
     if again == "Yes":
         print("\nHere..\n")
-        time.sleep(0.5)
+        time.sleep(0.3)
         print("We...\n")
-        time.sleep(0.5)
+        time.sleep(0.3)
         print("Go!\n")
         play_round(player_name)
     elif again == "No":
